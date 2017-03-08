6f466c60cc476e75b9cd8a98b83504214b98e5a5
人啊！！！ <s20151104690@1.local>
2017-03-08 10:37:28 +0800

    adapt
    add

--- add/ViewController.swift
+++ add/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func add(_ sender: Any) {
+        var temp:Int
+        temp = Int(x.text!)!+Int(y.text!)!
+        z.text="\(temp)"
+        
+    }
+    @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
